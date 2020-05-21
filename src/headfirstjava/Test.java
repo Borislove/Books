952153939b6901480099b8dba217567aaa2a00d6
Borislove <abirme@yandex.ru>
2020-05-21 20:16:26 +0300

    ...

---
+++ src/headfirstjava/Test.java
@@ -0,0 +1,12 @@
+package headfirstjava;
+
+//все никак не запомню...
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("System.out.println" + " вставляет перенос строки(printnewline) - напечатать новую строку");
+
+        System.out.print("System.out.print" + " продолжает выводить текст в той же строке");
+
+    }
+}
