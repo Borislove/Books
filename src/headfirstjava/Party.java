765588e79657fd85e27c135bd738945a9a3bd998
Borislove <abirme@yandex.ru>
2020-05-21 19:55:36 +0300

    head first...

---
+++ src/headfirstjava/Party.java
@@ -0,0 +1,38 @@
+package headfirstjava;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+//стр. 33
+public class Party {
+
+    public static void buildinvite() {
+        Frame f = new Frame();
+
+        Label l = new Label("Вечеринка у Тима");
+        Button b = new Button("Ваша ставка");
+        Button c = new Button("Сбросить");
+        Panel p = new Panel();
+
+        //фрейм + панель
+        f.add(p);
+        //на панель кнопочки и лейб
+        p.add(l);
+        p.add(b);
+        p.add(c);
+        //размеры
+        f.setSize(300, 200);
+        //упаковка - компоновка
+        f.pack();
+        //центр
+        f.setLocationRelativeTo(null);
+        //видимость окна
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Party.buildinvite();
+    }
+}
+
