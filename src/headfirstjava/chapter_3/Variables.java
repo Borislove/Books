c86b319a6ebe104bd0d71ef2f6345dacc9138096
Borislove <abirme@yandex.ru>
2020-05-29 06:00:20 +0300

    Head First (использование ссылок, переменные, массив(перебор))

---
+++ src/headfirstjava/chapter_3/Variables.java
@@ -0,0 +1,18 @@
+package headfirstjava.chapter_3;
+
+public class Variables {
+    public static void main(String[] args) {
+        int size = 32; //Объявляем переменную типа int  с именем size, присваиваем ей значение 32.
+
+        char initial = 'j'; // Объявляем переменную типа char с именем initial, присваиваем ей значение 'j'.
+
+        double d = 456.709; //Объявляем переменную типа double с именем d, присваиваем ей значение 456.709.
+
+        boolean isCrazy; //Объявляем переменную типа boolean с именем isCrazy(ничего не присваиваем).
+
+        isCrazy = true; //Присваиваем значение true объявленной ранее переменной isCrazy.
+
+        int x = 3;
+        int y = x + 456; //Объявляем переменную типа int с именем y , присваиваем ей значение, которой является суммой x и 456
+    }
+}
