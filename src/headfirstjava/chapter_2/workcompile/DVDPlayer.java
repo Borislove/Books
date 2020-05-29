58bd0160b31c4bc2956df1149cebbf1f74bfc44a
Borislove <abirme@yandex.ru>
2020-05-29 03:51:12 +0300

    Head First (тесты)

---
+++ src/headfirstjava/chapter_2/workcompile/DVDPlayer.java
@@ -0,0 +1,27 @@
+package headfirstjava.chapter_2.workcompile;
+
+public class DVDPlayer {
+
+    boolean canRecord = false;
+
+    void recordDVD() {
+        System.out.println("идет запись DVD");
+    }
+
+    void playDVD() {
+        System.out.println("идет проигрывание DVD");
+    }
+}
+
+
+class DVDPlayerTestDrive {
+    public static void main(String[] args) {
+        DVDPlayer d = new DVDPlayer();
+        d.canRecord = true;
+        d.playDVD();
+
+        if (d.canRecord == true) {
+            d.recordDVD();
+        }
+    }
+}
